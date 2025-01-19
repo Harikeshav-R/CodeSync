9e6967bbbb18859cba7e05a017be5da1f19cdc32
Harikeshav R <r.harikeshav@gmail.com>
2025-01-18 19:51:57 -0500

    Create docstrings for the command line functions

--- CodeSync/commands.h
+++ CodeSync/commands.h
@@ -5,6 +5,17 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
+
+struct cmd_struct
+{
+    const char* cmd;
+
+    int (* fn)(int, const char**);
+};
+
+
 int cmd_add(int argc, const char* argv[]);
 
 int cmd_cat_file(int argc, const char* argv[]);
@@ -17,6 +28,19 @@ int cmd_commit(int argc, const char* argv[]);
 
 int cmd_hash_object(int argc, const char* argv[]);
 
+
+/**
+ * Initializes a new repository at the specified path.
+ *
+ * This function parses command line arguments to obtain the path where the repository
+ * should be created. If the path is not provided, an error message is displayed, and
+ * the function returns an exit failure code. If the path is provided, the repository
+ * is created at that location.
+ *
+ * @param argc The number of command-line arguments.
+ * @param argv The array of command-line argument strings.
+ * @return 0 on success, EXIT_FAILURE if an error occurs (e.g., missing path argument).
+ */
 int cmd_init(int argc, const char* argv[]);
 
 int cmd_log(int argc, const char* argv[]);
